56e3b33b0f89e851c097f6b8b09efbfc82c1d91a
Thiago Fernandes Dias <thiago.fernandesdias235@gmail.com>
2024-09-11 12:20:46 -0300

    Utilizando NajaCompilerServiceInterface no CompilerController.

--- restapi/src/main/java/com/example/naja/restapi/controllers/CompilationsController.java
+++ restapi/src/main/java/com/example/naja/restapi/controllers/CompilationsController.java
@@ -1,5 +1,7 @@
 package com.example.naja.restapi.controllers;
 
+import com.example.naja.restapi.Enums.SupportedLanguages;
+import com.example.naja.restapi.services.NajaCompilerServiceInterface;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,18 +9,33 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Objects;
+
 @RestController
 public class CompilationsController {
+    private final NajaCompilerServiceInterface najaCompilerService;
+    
+    private CompilationsController(NajaCompilerServiceInterface najaCompilerService) {
+        this.najaCompilerService = najaCompilerService;
+    }
+
     @PostMapping("/compile")
     public ResponseEntity<String> compileToJava(@RequestParam("file") MultipartFile file,
             @RequestParam(value = "lang", defaultValue = "java") String lang) {
-        if (file.getContentType() != "text/plain") {
+        if (!Objects.equals(file.getContentType(), "text/plain")) {
             return new ResponseEntity<String>("Invalid file type. Only text/plain files are allowed.",
                     HttpStatus.BAD_REQUEST);
         }
         if (lang != "python" || lang != "java") {
             return new ResponseEntity<String>("Supported languages: java, python", HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<String>(HttpStatus.OK);
+        try {
+            String compiledCode = this.najaCompilerService.generateTarget(file.getInputStream(), SupportedLanguages.JAVA);
+            return new ResponseEntity<String>(compiledCode, HttpStatus.OK);    
+        }
+        catch (Exception e){
+            return new ResponseEntity<String>("Server internal error", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        
     }
 }
--- restapi/src/main/java/com/example/naja/restapi/services/NajaCompilerServiceImpl.java
+++ restapi/src/main/java/com/example/naja/restapi/services/NajaCompilerServiceImpl.java
@@ -3,7 +3,9 @@ package com.example.naja.restapi.services;
 import java.io.IOException;
 import java.io.InputStream;
 
+import com.example.naja.core.lib.NajaGrammarParser;
 import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
 import org.springframework.stereotype.Service;
 
 import com.example.naja.core.lib.NajaGrammarLexer;
@@ -15,17 +17,15 @@ public class NajaCompilerServiceImpl implements NajaCompilerServiceInterface {
     public String generateTarget(InputStream inputStream, SupportedLanguages language) {
         try {
             NajaGrammarLexer lexer = new NajaGrammarLexer(CharStreams.fromStream(inputStream));
+            CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+            NajaGrammarParser parser = new NajaGrammarParser(tokenStream);
+            System.out.println("UFABC Compiler");
+            parser.programa();
+            System.out.println("Compilation Successfully - Good Job");
+            return "Works";
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        // CommonTokenStream tokenStream = new CommonTokenStream(lexer);
-        // NajaGrammarParser parser = new NajaGrammarParser(tokenStream);
-
-        // agora eu quero chamar do meu jeito
-        System.out.println("UFABC Compiler");
-        // parser.programa();
-        System.out.println("Compilation Successfully - Good Job");
-        return "Works";
+        return "dont work";
     }
 }
